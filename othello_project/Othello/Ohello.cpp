3609cfd9bc37bac0b7a56bf86e02d9c93cc389c3
yusukekobayashi <yusuke_kobayashi@ewest.co.jp>
2019-12-06 16:29:59 +0900

    12月6日 インターンシップ成果物

--- othello_project/Othello/Ohello.cpp
+++ othello_project/Othello/Ohello.cpp
@@ -1,9 +1,5 @@
 ﻿#include "OthelloLib.h"
 
-void printBoardInfo(int* pBoard);
-void printSearchResult(int** pBoard, size_t size);
-void getStoneStr(char* pStr, size_t size, int stone);
-
 int main() {
 
 	//配列の要素数の設定
@@ -13,15 +9,18 @@ int main() {
 
 	//縦、横の配置指定(構造体)
 	struct POINT position;
-	//struct POINT* directions[HEIGHT][WIDTH] = {0};
 
 	//現在のプレイヤー(1 = ●、2 = ◯)
-	int player = 1;
+	int player = 1; 
+
+	//パス入力用
+	int pass = 1;
+
+	// 挟める石の数(0 = 挟む石が無い 0以上 = 挟む石がある)
+	int rbCount;
 
 	//石の初期配置
 	initializeBoard(board, WIDTH * HEIGHT);
-
-	//setDirection(directions[position.x][position.y], 8);
 
 	//盤面の表示
 	printBoard(board, WIDTH * HEIGHT);
@@ -34,26 +33,40 @@ int main() {
 
 	while (true)
 	{
+		rbCount = 0;
+
 		if (player == 1) {
-			printf("現在のプレイヤー:%s\n", BLACK_STONE_STR);
-			player = 2;
-			playStone = BLACK_STONE;
+			if (pass == 0) {
+				pass = 1;
+			}
+			else if (pass == 1)
+			{
+				printf("現在のプレイヤー:%s\n", BLACK_STONE_STR);
+				player = 2;
+				playStone = BLACK_STONE;
+			}
 		}
 		else if (player == 2)
 		{
-			printf("現在のプレイヤー:%s\n", WHITE_STONE_STR);
-			player = 1;
-			playStone = WHITE_STONE;
+			if (pass == 0) {
+				pass = 1;
+			}
+			else if (pass == 1)
+			{
+				printf("現在のプレイヤー:%s\n", WHITE_STONE_STR);
+				player = 1;
+				playStone = WHITE_STONE;
+			}
 		}
 
 		while (true) {
 			//配置場所の指定
 			printf("石の位置を指定してください。\n");
-			printf("横:"); scanf_s("%d", &position.y);
-			printf("縦:"); scanf_s("%d", &position.x);
+			printf("横:"); scanf_s("%d", &position.x);
+			printf("縦:"); scanf_s("%d", &position.y);
 
 			if ((position.x > 7 || position.y > 7) || (position.x < 0 || position.y < 0)) { //盤面の範囲外の場合
-				printf("盤面の範囲外です、違う場所を指定ください。");
+				printf("盤面の範囲外です、違う場所を指定してください。");
 			}
 			else {
 				break;
@@ -74,67 +87,82 @@ int main() {
 		int initX = playPoint.x + Direction.x;
 
 		// 探索
-		bool isFound = false;
 		int reverseBoardCount = 0;
-		int* pReverseBoard[WIDTH * HEIGHT] = { NULL }; // 余裕を持った領域にしておく
+
+		//探索用(挟む石がない = false、ある = true)
+		bool isFound = false;
+
 		for (int y = initY, x = initX;
 			(y >= 0 && y < HEIGHT) && (x >= 0 && x < WIDTH);
 			y += Direction.y, x += Direction.x) {
 
 			if (pboard[(x * 8) + y] == NONE_PLAY) {
 				// 未プレイ
-				continue;
+				break;
 			}
 			if (pboard[(x * 8) + y] == playStone) {
 				// プレイヤー自身の石を発見
 				isFound = true;
+
+				//隣にプレイヤー自身の石があった場合
+			    /*if (pboard[((x - Direction.x) * 8) + (y - Direction.y)] == playStone) {
+					rbCount++;
+				}*/
+
 				break;
 			}
-			// 対戦相手の石で、反転対象となる
-			pReverseBoard[reverseBoardCount] = &pboard[(x * 8) + y];
-			// 盤面上の石反転
-			if (player == BLACK_STONE) {
-				board[(x * 8) + y] = BLACK_STONE;
-			}
-			else if (player == WHITE_STONE){
-				board[(x * 8) + y] = WHITE_STONE;
-			}
+
 			reverseBoardCount++;
+			rbCount++;
 		}
 
-		if (isFound) {
-			// 探索結果の出力(テスト用)
-			printSearchResult(pReverseBoard, reverseBoardCount);
+		if (isFound == true) {
+			int ix = initX;
+			int iy = initY;
+			for (int i = 0; i < reverseBoardCount; i++) {
+				if (player == BLACK_STONE) {
+					board[(ix * 8) + iy] = BLACK_STONE;
+				}
+				else if (player == WHITE_STONE) {
+					board[(ix * 8) + iy] = WHITE_STONE;
+				}
+
+				ix += Direction.x;
+				iy += Direction.y;
+			}
 		}
-		else {
-			printf("\n【探索結果なし】\n");
-		}
+
 	  }
-		printBoard(board, WIDTH * HEIGHT);
+ 
+	  if(rbCount == 0)  //挟む石が無い
+	  {
+		  while (true) {
+			  printf("挟む石がありません。パス(0:しない、1:する)しますか?\n");
+			  scanf_s("%d", &pass);
+
+			  if (pass > 1 || pass < 0) {
+				  printf("0か1を入力してください。\n");
+			  }
+			  else {
+				  break;
+			  }
+		  }
+	  }
+	  else if (rbCount > 0) { //挟む石がある
+
+		  //石を盤面に配置
+		  if (playStone == BLACK_STONE) {
+			  board[(position.x * 8) + position.y] = BLACK_STONE;
+		  }
+		  else if (playStone == WHITE_STONE)
+		  {
+			  board[(position.x * 8) + position.y] = WHITE_STONE;
+		  }
+
+		  //反転後の盤面表示
+		  printBoard(board, WIDTH * HEIGHT);
+	  }
 	}
 
 	return 0;
-}
-
-// テスト用
-void printSearchResult(int** pBoard, size_t size) {
-	printf("\n【探索した盤面情報】\n");
-	printf("===================\n");
-	printf("       石  アドレス\n");
-	printf("===================\n");
-	for (unsigned int i = 0; i < size; i++) {
-		char stoneStr[10] = { '\0' };
-		getStoneStr(stoneStr, sizeof(stoneStr) / sizeof(char), (*pBoard[i]));
-		printf("       %s  %p\n", stoneStr, pBoard[i]);
-	}
-	printf("===================\n");
-}
-
-void getStoneStr(char* pStr, size_t size, int stone) {
-	if (stone == BLACK_STONE) {
-		sprintf_s(pStr, size, "%s", BLACK_STONE_STR);
-	}
-	else{
-		sprintf_s(pStr, size, "%s", WHITE_STONE_STR);
-	}
 }
